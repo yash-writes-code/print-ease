17fbee45415bc8b75780d2fdced2b4372452ad86
Yash Goyal <yashgoyalambk@gmail.com>
2025-04-07 23:56:05 +0530

    updated otp system, removed unneccasary stuff
    Signed-off-by: Yash Goyal <yashgoyalambk@gmail.com>

--- src/utils/generate_otp.ts
+++ src/utils/generate_otp.ts
@@ -1,25 +1,17 @@
 import clientPromise from "@/lib/db";
 
-
 const client = await clientPromise;
 const db = client.db("PrintEase");
 
-
 export async function generate_otp() {
-    const OtpCollection = db.collection("Otp");
+    //alternate way to find current otp is to juist find the last printdoc instance and update it
+    const PrintDocCollection = db.collection("PrintDoc");
 
-    // Try to find the OTP document
-    const otpDoc = await OtpCollection.findOne({key:"current_otp"});
+    const last_pd = await PrintDocCollection.find().sort({createdAt:-1}).limit(1).next();
 
-    const currentOtp = otpDoc?.otp || "A0"; // Default to A0 if not found
+    const currentOtp = last_pd?.otp?? "A0";
+
     const newOtp = incrementOtp(currentOtp);
-
-    // Upsert the document (insert if not exists, update if exists)
-    await OtpCollection.updateOne(
-        { key: "current_otp" },
-        { $set: { otp: newOtp } },
-        { upsert: true }
-    );
 
     return newOtp;
 }
@@ -38,22 +30,4 @@ function incrementOtp(otp: string): string {
     return letter + number;
 }
 
-
-function incrementLetterLoop(letter:string) {
-    return letter === 'Z' ? 'A' : String.fromCharCode(letter.charCodeAt(0) + 1);
-}
-
-function increment_otp(current_otp:string){
-    let [letter,number]=[current_otp[0],current_otp.slice(1)];
-    
-    if(eval(number) == 99){
-        letter=incrementLetterLoop(letter);
-        number="0";
-    }
-    else{
-        number = String(eval(number)+1);
-    };
-    current_otp=letter+number;
-    return current_otp;
-}
 
